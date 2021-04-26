e4e26c8df52192f3c8c85a7ac2661c8013a118bf
eapg <elidopg@gmail.com>
2021-04-26 16:05:47 -0400

    Order class added

---
+++ scr/orde_class.py
@@ -0,0 +1,10 @@
+# class for the order, it will contain what the client order, and its status.
+
+class Order:
+
+    Order_list = []
+    Order_status = str
+
+    def __init__(self):
+        pass
+
