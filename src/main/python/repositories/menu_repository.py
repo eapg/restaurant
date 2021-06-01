72324c2646591c694c32ec31f30760d31f94de1b
eapg <elidopg@gmail.com>
2021-06-01 15:55:09 -0400

    Menu Repository Added

---
+++ src/main/python/repositories/menu_repository.py
@@ -0,0 +1,28 @@
+# This file has the menu repository
+
+from src.main.python.repositories.generic_repository import GeneralRepository
+
+
+class MenuRepository(GeneralRepository):
+
+    def __init__(self):
+        self.menus = {}
+        self.next_id = 1
+
+    def add(self, menu):
+        menu.id = self.next_id
+        self.menus[self.next_id] = menu
+        self.next_id += 1
+        return menu
+
+    def get_by_id(self, menu_id):
+        return self.menus[menu_id]
+
+    def get_all(self):
+        return self.menus
+
+    def delete_by_id(self, menu_id):
+        return self.menus.pop(menu_id)
+
+    def update_by_id(self, menu_id, new_menu):
+        self.menus[menu_id] = new_menu
