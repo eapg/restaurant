251fa3e81be057bfc68d1c56347ae7045e04e3bc
eapg <elidopg@gmail.com>
2021-06-05 21:05:36 -0400

    Menu Controller Added

---
+++ src/main/python/controller/menu_controller.py
@@ -0,0 +1,23 @@
+# structure for the menu controller
+
+
+class MenuController:
+
+    def __init__(self, menu_repository):
+        self.menu_repository = menu_repository
+
+    def get_menu(self, menu_id):
+        self.menu_repository.get_by_id(menu_id)
+
+    def get_menus(self):
+        self.menu_repository.get_all()
+
+    def insert_menu(self, menu):
+        self.menu_repository.add(menu)
+
+    def update_menu_by_id(self, menu_id):
+        self.menu_repository.update_by_id(menu_id)
+
+    def delete_menu_by_id(self, menu_id):
+        self.menu_repository.delete_by_id(menu_id)
+
