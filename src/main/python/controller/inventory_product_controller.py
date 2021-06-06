af79cf45004aa5b7c9d74fb588a47a80a587d5b9
eapg <elidopg@gmail.com>
2021-06-05 20:59:48 -0400

    Inventory Product Controller Added

---
+++ src/main/python/controller/inventory_product_controller.py
@@ -0,0 +1,23 @@
+# structure for the inventory product controller
+
+
+class InventoryProductController:
+
+    def __init__(self, inventory_product_repository):
+        self.inventory_product_repository = inventory_product_repository
+
+    def get_inventory_product(self, inventory_product_id):
+        self.inventory_product_repository.get_by_id(inventory_product_id)
+
+    def get_inventories_product(self):
+        self.inventory_product_repository.get_all()
+
+    def insert_inventory_product(self, inventory_product):
+        self.inventory_product_repository.add(inventory_product)
+
+    def update_inventory_product_by_id(self, inventory_product_id):
+        self.inventory_product_repository.update_by_id(inventory_product_id)
+
+    def delete_inventory_product_by_id(self, inventory_product_id):
+        self.inventory_product_repository.delete_by_id(inventory_product_id)
+
